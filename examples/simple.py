252f2d9cc8be5e7374259eddd447d366b329b86d
Ken Dreyer <kdreyer@ibm.com>
2023-08-09 12:42:45 -0400

    examples: update "far away" date to 2025

--- examples/simple.py
+++ examples/simple.py
@@ -2,7 +2,7 @@ import datetime
 from rhcalendar import networkdays
 
 today = datetime.date.today()
-to_date = datetime.date(2020, 1, 1)  # something far away...
+to_date = datetime.date(2025, 1, 1)  # something far away...
 
 days = networkdays(from_date=today, to_date=to_date)
 
