d4b365bc7639704e0d575d4d6b43d79799915610
Ken Dreyer <kdreyer@redhat.com>
2025-02-25 09:10:11 -0500

    tests: test most recent year
    Test the 2025 holidays I added recently

--- rhcalendar/tests/test_en_us.py
+++ rhcalendar/tests/test_en_us.py
@@ -3,7 +3,7 @@ from rhcalendar import networkdays
 
 
 def test_en_us():
-    from_date = datetime.date(2023, 12, 28)
-    to_date = datetime.date(2023, 12, 29)
+    from_date = datetime.date(2025, 12, 28)
+    to_date = datetime.date(2025, 12, 29)
     result = networkdays(from_date, to_date, locale='en-US')
     assert result == 0
