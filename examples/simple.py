f41984baf31befb55004a43bb13ace38465b0e6d
Ken Dreyer <kdreyer@redhat.com>
2025-02-25 09:11:06 -0500

    use example dates in the future
    Update the example script and README code to use modern dates in the
    future.

--- examples/simple.py
+++ examples/simple.py
@@ -2,8 +2,8 @@ import datetime
 from rhcalendar import networkdays
 
 today = datetime.date.today()
-to_date = datetime.date(2025, 1, 1)  # something far away...
+to_date = datetime.date(2026, 1, 1)  # something far away...
 
 days = networkdays(from_date=today, to_date=to_date)
 
-print('%s is %d Red Hat business days from now.' % (to_date, days))
+print(f'{to_date} is {days} Red Hat business days from now.')
